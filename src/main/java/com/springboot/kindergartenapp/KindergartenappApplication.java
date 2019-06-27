fb44e24d8f6b4a8e29015c7be64161f40413faf0
Yunus Halit <yhbaran@gmail.com>
2019-06-27 22:49:19 +0300

    Initial commit

---
+++ src/main/java/com/springboot/kindergartenapp/KindergartenappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kindergartenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindergartenappApplication.class, args);
+	}
+
+}
